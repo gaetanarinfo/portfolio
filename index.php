47e1cb832057e37d5abe162d74da8a31c819032b
gaetanarinfo <gaetanarinfo@gmail.com>
2020-12-15 14:33:29 +0100

    Site

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Portfolio de Gaëtan Seigneur</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <?php include_once 'view/header.php'; ?>
+
+    <?php include_once 'view/section/section1.php'; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'view/section/section2.php'; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'view/section/section3.php'; ?>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'view/section/section4.php'; ?>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'view/section/section5.php'; ?>
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'view/section/section6.php'; ?>
+
+    <?php include_once 'view/footer.php'; ?>
+
+    <?php include_once 'view/notification.php'; ?>
+
+    <?php include_once 'view/modal/modal_membre.php'; ?>
+
+    <!-- js icons -->
+    <script src="assets/vendor/icons/9d1d83a1dd.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="assets/vendor/jquery/jquery-3.5.1.slim.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/popper.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
